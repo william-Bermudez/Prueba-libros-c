f8c31b77d3e4b8ffbd8b4903d2f73a226fda1780
William Josue Bermudez Flores <wiliangamer838@gmail.com>
2026-09-18 09:36:01 -0600

    Agregar archivos de proyecto.

---
+++ Prueba/Models/Libro.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+namespace Prueba.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Titulo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Autor { get; set; }
+        [Required]
+        [StringLength(13, MinimumLength = 10)]
+        public string ISBN { get; set; }
+
+        [Precision(18, 2)]
+        public decimal precio { get; set; }
+
+
+        public bool disponibilidad { get; set; }
+
+
+        //el codigo sera public DbSet<Libro> Libro { get; set; } 
+
+    }
+}
